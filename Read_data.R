0ed3f38db4f337d18e7ee191b6d28fe5463babd1
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2023-04-12 17:00:52 +0200

    Data read now includes Lyon and monthly data for 2016-2022

--- Read_data.R
+++ Read_data.R
@@ -3,41 +3,84 @@
 
 library(httr)
 library(glue)
+library(rrapply)
+library(tidyr)
 
 dir.create("raw_data", recursive = TRUE, showWarnings = FALSE)
 
+#### Define functions ####
 
-WEATHER_STATION = "FR33203"
-START_DATE = "2020-01-01"
-END_DATE = "2021-12-31"
+allMerge <- function(x, y) merge(x, y, all = TRUE)
 
-pollute_map <- c("03" = "dioxyde_azote",
-                 "24" = "PM10",
-                 "02" = "monoxyde_azote"
-                 )
+nullToNA <- function(x){
+  if (is.null(x)) NA else x
+}
 
-pollute_list <- vector(mode = "list", length(pollute_map))
+#### 
 
-for(pollute_id in seq_along(pollute_map)){
+START_YEAR = 2015
+END_YEAR = 2022
+
+weather_stations = read.csv(text = 
+"city,station
+Annecy,FR33203
+Lyon,FR20062"
+)
+
+pollutants = read.csv(text = 
+"id,pollutant
+03,dioxyde_azote
+24,PM10
+02,monoxyde_azote",
+colClasses = "character")
+
+years <- data.frame(year = START_YEAR:END_YEAR)
+
+pollute_map <- Reduce(allMerge, list(weather_stations, pollutants, years))
+
+pollute_list <- vector(mode = "list", nrow(pollute_map))
+
+for(pollute_index in seq_len(nrow(pollute_map))){
   
-  pollute_num <- names(pollute_map)[pollute_id]
-  pollute_name <- pollute_map[pollute_id]
   
-  url <- glue("https://www.atmo-auvergnerhonealpes.fr/dataviz/dataviz/mesures/{WEATHER_STATION}/{pollute_num}/{START_DATE}/{END_DATE}")
-  
-  resp_list <- content(GET(url))
-  df_list <- lapply(resp_list, as.data.frame)
-  out <- do.call(rbind, df_list)
-  
-  out <- out[,c("date", "pollutant")]
-  names(out) <- c("date", pollute_name)
-  
-  pollute_list[[pollute_id]] <- out
+  pollute_list[[pollute_index]] <- 
+    with(pollute_map[pollute_index,],{
+      
+      message(glue("[{Sys.time()}] Station: {station} | Pollutant: {pollutant} | Year : {year}"))
+      
+      url <- glue("https://www.atmo-auvergnerhonealpes.fr/dataviz/dataviz/mesures/{station}/{id}/{year}-01-01/{year}-12-31")
+      
+      resp_list <- content(GET(url))
+      resp_list <- rrapply(resp_list,is.null, \(x) NA)
+      df_list <- lapply(resp_list, as.data.frame)
+      out <- do.call(rbind, df_list)
+      
+      
+      out$city <- city
+      out$station <- station
+      out$pollutant_level <- out$pollutant
+      out$pollutant_name <- pollutant
+      
+      out <- out[,c("date", "city", "station", "pollutant_name", "pollutant_level")]
+      
+      pollute_list[[pollute_index]] <- out
+      
+      
+    }
+       
+       
+       )
   
 }
 
-final_data <- Reduce(\(x, y) merge(x, y, all = TRUE), pollute_list)
+final_data <- Reduce(allMerge, pollute_list)
 
+# convert millisecond timestamp to date
 final_data$date <- as.Date(as.POSIXct(final_data$date/1000, origin="1970-01-01"))
 
-write.csv(final_data, "raw_data/annecy_airquality.csv", row.names = FALSE)
+final_data %>%
+  pivot_wider(id_cols = date:station, names_from = pollutant_name, values_from = pollutant_level) ->
+  wide_data
+
+
+write.csv(wide_data, "raw_data/annecy_airquality.csv", row.names = FALSE)
